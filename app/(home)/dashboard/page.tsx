c5eb973a683a08a79d70dbbe8598ff26281b804f
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2025-03-25 14:15:11 +0600

    update

--- app/(home)/dashboard/page.tsx
+++ app/(home)/dashboard/page.tsx
@@ -1,9 +1,10 @@
-import { getCookies } from "@/app/actions/actions";
 import DashboardBody from "@/components/dashboard/dashboard-body";
 import { PageHeader } from "@/components/page-header";
+import { cookies } from "next/headers";
 
 export default async function DashboardPage() {
-  const token = await getCookies();
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value || null;
 
   return (
     <div className="flex min-h-screen flex-col py-8 pt-6">
